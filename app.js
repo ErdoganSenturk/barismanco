1b7472b3528ac507750d71ed431fde6cf72dfb12
ErdoganSenturk <erdsen57@gmail.com>
2022-06-03 20:20:16 +0300

    add event

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-document.getElementById("click").onclick = function () {
-  document.getElementById("barismanco").style.webkitTextFillColor="green"
-};
+
+
+
+
+
+let bcolor = document.getElementById("barismanco");
+
+
+bcolor.addEventListener("click", () =>{
+
+  let  randomcolor = "#" + Math.floor(Math.random() * 16777215 ).toString(16);
+  bcolor.style.webkitTextFillColor = randomcolor
+
+} );
